f09600c72543908dadbd6e6a06405fd1f3b7fe4b
mrkirby153 <mr.austinwhyte@gmail.com>
2023-04-22 11:25:28 -0700

    Add apply script

---
+++ apply.sh
@@ -0,0 +1,31 @@
+#!/bin/env sh
+
+
+usage() {
+    echo "Usage: $0 [build|apply]" >&2
+}
+
+if [[ $# -eq 0 ]]; then
+    COMMAND=apply
+elif [[ $# -eq 1 ]]; then
+    COMMAND="$1"
+else
+    usage
+    exit 1
+fi
+
+
+case $COMMAND in
+    "apply")
+        CMD="switch"
+        ;;
+    "build")
+        CMD="build"
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
+home-manager --max-jobs auto "$CMD" --flake .
