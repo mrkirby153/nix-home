871b77be08ca296307f90aa88c2f142eb3e50fec
mrkirby153 <mr.austinwhyte@gmail.com>
2023-04-22 00:43:27 -0700

    Add home manager

---
+++ home.nix
@@ -0,0 +1,11 @@
+{ pkgs, ...}: {
+    home.username = "austin";
+    home.homeDirectory = "/home/austin";
+    home.stateVersion = "22.11";
+    programs.home-manager.enable = true;
+
+    home.packages = with pkgs; [
+        httpie
+        nix-prefetch-scripts
+    ];
+}
