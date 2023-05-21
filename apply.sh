df081619dc121af823ea0955e4f405db08c8dc25
mrkirby153 <mr.austinwhyte@gmail.com>
2023-05-21 14:52:44 -0700

    Configure nix

--- apply.sh
+++ apply.sh
@@ -32,8 +32,8 @@ actual_hostname="${HOSTNAME:-$(hostname)}"
 
 if ! type home-manager > /dev/null; then
     echo "home-manager not found, running via nix"
-    nix run . "$CMD" -- --flake ".#$actual_hostname"
+    nix run --extra-experimental-features "nix-command flakes" . "$CMD" -- --flake ".#$actual_hostname"
     exit 0
 fi
 
-home-manager --max-jobs auto "$CMD" --flake ".#$actual_hostname"
+home-manager --extra-experimental-features "nix-command flakes" --max-jobs auto "$CMD" --flake ".#$actual_hostname"
